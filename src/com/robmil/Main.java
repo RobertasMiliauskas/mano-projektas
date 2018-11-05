76306b153c89d8d522ee1baa1cd2cc1a583ca23b
RobertasMiliauskas <miliauskas.robertas@gmail.mcom>
2018-11-05 23:58:27 +0200

    code formating

--- src/com/robmil/Main.java
+++ src/com/robmil/Main.java
@@ -3,5 +3,6 @@ package com.robmil;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Labas labas labas");    }
+        System.out.println("Labas labas labas");
+    }
 }
